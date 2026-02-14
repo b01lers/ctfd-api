35397f441f7d42b5e6c649c82025332d4fd97b3f
Kevin Yu <kevinyu567@gmail.com>
2026-02-13 21:52:36 -0500

    Build library

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,147 @@
 'use strict';
 
+function createChallengesMethods(client) {
+  return {
+    /**
+     * Attempts to submit a flag for the given challenge.
+     * Ref: {@Link https://docs.ctfd.io/docs/api/redoc#tag/challenges/operation/post_challenge_attempt}
+     *
+     * @param id The ID of the challenge to submit a flag for.
+     * @param flag The flag to submit.
+     * @returns The status of the flag submission.
+     */
+    async submitFlag(id, flag) {
+      const { session, nonce } = await client.getAuthedSessionNonce();
+      const res = await (await fetch(`${client.url}/api/v1/challenges/attempt`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Csrf-Token": nonce,
+          cookie: session
+        },
+        body: JSON.stringify({ challenge_id: id, submission: flag })
+      })).json();
+      return res.data;
+    },
+    /**
+     * Fetches the list of all challenges.
+     * Ref: {@Link https://docs.ctfd.io/docs/api/redoc#tag/challenges/operation/get_challenge_list}
+     *
+     * @returns The list of challenges, as a `Challenge[]`.
+     */
+    async get() {
+      return client.callApi("/challenges");
+    },
+    /**
+     * Fetches details for the given challenge.
+     * Ref: {@Link https://docs.ctfd.io/docs/api/redoc#tag/challenges/operation/get_challenge}.
+     *
+     * @param id The ID of the challenge to fetch.
+     * @returns The challenge details.
+     */
+    async getDetails(id) {
+      return client.callApi(`/challenges/${id}`);
+    },
+    /**
+     * Fetches solves for the given challenge.
+     * Ref: {@Link https://docs.ctfd.io/docs/api/redoc#tag/challenges/operation/get_challenge_solves}.
+     *
+     * @param id The ID of the challenge to fetch solves for.
+     * @returns The challenge's solves.
+     */
+    async getSolves(id) {
+      return client.callApi(`/challenges/${id}/solves`);
+    }
+  };
+}
+
+function createScoreboardMethods(client) {
+  return {
+    /**
+     * Fetches the list of scoreboard entries.
+     * Ref: {@Link https://docs.ctfd.io/docs/api/redoc#tag/scoreboard/operation/get_scoreboard_list}
+     *
+     * @returns The scoreboard, as a `ScoreboardEntry[]`.
+     */
+    async get() {
+      return client.callApi("/scoreboard");
+    },
+    /**
+     * Fetches details for the top `n` teams on the scoreboard.
+     * Ref: {@Link https://docs.ctfd.io/docs/api/redoc#tag/scoreboard/operation/get_scoreboard_detail}
+     *
+     * @param count The number of teams to fetch.
+     * @returns The scoreboard details.
+     */
+    async getTop(count) {
+      return client.callApi(`/scoreboard/top/${count}`);
+    }
+  };
+}
+
+function createUsersMethods(client) {
+  return {
+    me: {
+      /**
+       * Retrieves the currently logged-in user.
+       * Ref: {@Link https://docs.ctfd.io/docs/api/redoc#tag/users/operation/get_user_private}
+       * @returns The logged-in user.
+       */
+      async get() {
+        return client.callApi("/users/me");
+      },
+      /**
+       * Retrieves solves for the currently logged-in user.
+       * Ref: {@Link https://docs.ctfd.io/docs/api/redoc#tag/users/operation/get_user_private_solves}
+       *
+       * @returns The logged-in user's solves.
+       */
+      async getSolves() {
+        return client.callApi("/users/me/solves");
+      },
+      /**
+       * Retrieves awards for the currently logged-in user.
+       * Ref: {@Link https://docs.ctfd.io/docs/api/redoc#tag/users/operation/get_user_private_awards}
+       *
+       * @returns The logged-in user's awards.
+       */
+      async getAwards() {
+        return client.callApi("/users/me/awards");
+      }
+    },
+    /**
+     * Retrieves a user by ID.
+     * Ref: {@Link https://docs.ctfd.io/docs/api/redoc#tag/users/operation/get_user_public}
+     *
+     * @param id The ID of the user to retrieve.
+     * @returns The fetched user.
+     */
+    async getById(id) {
+      return client.callApi(`/users/${id}`);
+    },
+    /**
+     * Retrieves solves for a given user.
+     * Ref: {@Link https://docs.ctfd.io/docs/api/redoc#tag/users/operation/get_user_public_solves}
+     *
+     * @param id The ID of the user to retrieve solves for.
+     * @returns The user's solves.
+     */
+    async getSolves(id) {
+      return client.callApi(`/users/${id}/solves`);
+    },
+    /**
+     * Retrieves awards for a given user.
+     * Ref: {@Link https://docs.ctfd.io/docs/api/redoc#tag/users/operation/get_user_public_awards}
+     *
+     * @param id The ID of the user to retrieve awards for.
+     * @returns The user's awards.
+     */
+    async getAwards(id) {
+      return client.callApi(`/users/${id}/awards`);
+    }
+  };
+}
+
 function extractNonce(raw) {
   return raw.match(/'csrfNonce': "(.+?)"/)[1];
 }
@@ -15,40 +157,19 @@ class CTFdClient {
     __publicField(this, "cachedSession", null);
     __publicField(this, "cachedNonce", null);
     __publicField(this, "sessionExpiry", /* @__PURE__ */ new Date());
+    __publicField(this, "challenges");
+    __publicField(this, "scoreboard");
+    __publicField(this, "users");
     this.url = options.url.endsWith("/") ? options.url.slice(0, -1) : options.url;
     this.username = options.username;
     this.password = options.password;
+    this.challenges = createChallengesMethods(this);
+    this.scoreboard = createScoreboardMethods(this);
+    this.users = createUsersMethods(this);
   }
-  async submitFlag(id, flag) {
+  async callApi(endpoint) {
     const { session, nonce } = await this.getAuthedSessionNonce();
-    const res = await (await fetch(`${this.url}/api/v1/challenges/attempt`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "Csrf-Token": nonce,
-        cookie: session
-      },
-      body: JSON.stringify({ challenge_id: id, submission: flag })
-    })).json();
-    return res.data;
-  }
-  async getChallenges() {
-    const { session } = await this.getAuthedSessionNonce();
-    const res = await (await fetch(`${this.url}/api/v1/challenges`, {
-      headers: { cookie: session }
-    })).json();
-    return res.data;
-  }
-  async getChallengeDetails(id) {
-    const { session } = await this.getAuthedSessionNonce();
-    const res = await (await fetch(`${this.url}/api/v1/challenges/${id}`, {
-      headers: { cookie: session }
-    })).json();
-    return res.data;
-  }
-  async getScoreboard() {
-    const { session, nonce } = await this.getAuthedSessionNonce();
-    const res = await (await fetch(`${this.url}/api/v1/scoreboard`, {
+    const res = await (await fetch(`${this.url}/api/v1${endpoint}`, {
       headers: {
         "Csrf-Token": nonce,
         cookie: session
@@ -56,22 +177,23 @@ class CTFdClient {
     })).json();
     return res.data;
   }
+  // TODO
   async getAuthedSessionNonce() {
     if (/* @__PURE__ */ new Date() < this.sessionExpiry && this.cachedSession && this.cachedNonce)
       return { session: this.cachedSession, nonce: this.cachedNonce };
     const res = await fetch(`${this.url}/login`);
     const [session] = res.headers.getSetCookie()[0].split("; ");
     const nonce = extractNonce(await res.text());
-    const formData = new URLSearchParams();
-    formData.append("name", this.username);
-    formData.append("password", this.password);
-    formData.append("_submit", "Submit");
-    formData.append("nonce", nonce);
+    const params = new URLSearchParams();
+    params.append("name", this.username);
+    params.append("password", this.password);
+    params.append("_submit", "Submit");
+    params.append("nonce", nonce);
     const loginRes = await fetch(`${this.url}/login`, {
       method: "POST",
       headers: { cookie: session },
       redirect: "manual",
-      body: formData
+      body: params
     });
     const [authedSession, expiresGmt] = loginRes.headers.getSetCookie()[0].split("; ");
     const authedRaw = await (await fetch(`${this.url}/challenges`, {
--- dist/types.d.ts
+++ dist/types.d.ts
@@ -1,30 +1,11 @@
-type BaseScoreboardEntry = {
-    pos: number;
+interface Solve {
     account_id: number;
     account_url: string;
-    oauth_id: null;
+    date: string;
     name: string;
-    score: number;
-    bracket_id: number | null;
-    bracket_name: string | null;
-};
-type ScoreboardUserEntry = BaseScoreboardEntry & {
-    account_type: "user";
-};
-type ScoreboardTeamEntry = BaseScoreboardEntry & {
-    account_type: "team";
-    members: {
-        id: number;
-        oauth_id: null;
-        name: string;
-        score: number;
-        bracket_id: null;
-        bracket_name: null;
-    }[];
-};
-type ScoreboardEntry = ScoreboardUserEntry | ScoreboardTeamEntry;
+}
 type ChallengeType = 'standard' | 'multiple_choice' | 'code';
-type ChallengeData = {
+interface Challenge {
     id: number;
     type: ChallengeType;
     name: string;
@@ -35,7 +16,7 @@ type ChallengeData = {
     tags: string[];
     template: string;
     value: number;
-};
+}
 type BaseChallengeDetails = {
     id: number;
     name: string;
@@ -80,19 +61,15 @@ type ProgrammingChallengeDetails = BaseChallengeDetails & {
 };
 type ChallengeDetails = StandardChallengeDetails | ProgrammingChallengeDetails;
 
-type ClientOptions = {
-    url: string;
-    username: string;
-    password: string;
-};
-declare class CTFdClient {
-    private readonly url;
-    private readonly username;
-    private readonly password;
-    private cachedSession;
-    private cachedNonce;
-    private sessionExpiry;
-    constructor(options: ClientOptions);
+declare function createChallengesMethods(client: CTFdClient): {
+    /**
+     * Attempts to submit a flag for the given challenge.
+     * Ref: {@Link https://docs.ctfd.io/docs/api/redoc#tag/challenges/operation/post_challenge_attempt}
+     *
+     * @param id The ID of the challenge to submit a flag for.
+     * @param flag The flag to submit.
+     * @returns The status of the flag submission.
+     */
     submitFlag(id: number, flag: string): Promise<{
         status: "incorrect";
         message: "Incorrect";
@@ -109,10 +86,206 @@ declare class CTFdClient {
         status: "ratelimited";
         message: "You're submitting flags too fast. Slow down.";
     }>;
-    getChallenges(): Promise<ChallengeData[]>;
-    getChallengeDetails(id: number): Promise<ChallengeDetails>;
-    getScoreboard(): Promise<ScoreboardEntry[]>;
-    private getAuthedSessionNonce;
+    /**
+     * Fetches the list of all challenges.
+     * Ref: {@Link https://docs.ctfd.io/docs/api/redoc#tag/challenges/operation/get_challenge_list}
+     *
+     * @returns The list of challenges, as a `Challenge[]`.
+     */
+    get(): Promise<Challenge[]>;
+    /**
+     * Fetches details for the given challenge.
+     * Ref: {@Link https://docs.ctfd.io/docs/api/redoc#tag/challenges/operation/get_challenge}.
+     *
+     * @param id The ID of the challenge to fetch.
+     * @returns The challenge details.
+     */
+    getDetails(id: number): Promise<ChallengeDetails>;
+    /**
+     * Fetches solves for the given challenge.
+     * Ref: {@Link https://docs.ctfd.io/docs/api/redoc#tag/challenges/operation/get_challenge_solves}.
+     *
+     * @param id The ID of the challenge to fetch solves for.
+     * @returns The challenge's solves.
+     */
+    getSolves(id: number): Promise<Solve[]>;
+};
+
+type BaseScoreboardEntry = {
+    pos: number;
+    account_id: number;
+    account_url: string;
+    oauth_id: null;
+    name: string;
+    score: number;
+    bracket_id: number | null;
+    bracket_name: string | null;
+};
+type ScoreboardUserEntry = BaseScoreboardEntry & {
+    account_type: "user";
+};
+type ScoreboardTeamEntry = BaseScoreboardEntry & {
+    account_type: "team";
+    members: {
+        id: number;
+        oauth_id: null;
+        name: string;
+        score: number;
+        bracket_id: null;
+        bracket_name: null;
+    }[];
+};
+type ScoreboardEntry = ScoreboardUserEntry | ScoreboardTeamEntry;
+interface ScoreboardDetails {
+    id: number;
+    account_url: string;
+    name: string;
+    score: number;
+    bracket_id: null;
+    bracket_name: null;
+    solves: {
+        challenge_id: number;
+        account_id: number;
+        team_id: number | null;
+        user_id: number;
+        value: number;
+        date: string;
+    }[];
 }
 
-export { CTFdClient, type ChallengeData, type ChallengeDetails, type ChallengeType, type ScoreboardEntry };
+declare function createScoreboardMethods(client: CTFdClient): {
+    /**
+     * Fetches the list of scoreboard entries.
+     * Ref: {@Link https://docs.ctfd.io/docs/api/redoc#tag/scoreboard/operation/get_scoreboard_list}
+     *
+     * @returns The scoreboard, as a `ScoreboardEntry[]`.
+     */
+    get(): Promise<ScoreboardEntry[]>;
+    /**
+     * Fetches details for the top `n` teams on the scoreboard.
+     * Ref: {@Link https://docs.ctfd.io/docs/api/redoc#tag/scoreboard/operation/get_scoreboard_detail}
+     *
+     * @param count The number of teams to fetch.
+     * @returns The scoreboard details.
+     */
+    getTop(count: number): Promise<ScoreboardDetails[]>;
+};
+
+interface User {
+    id: number;
+    oauth_id: number | null;
+    name: string;
+    email?: string;
+    website: string | null;
+    affiliation: string | null;
+    country: string | null;
+    bracket_id: number | null;
+    language: string | null;
+    team_id: number | null;
+    created: string;
+    fields: [];
+}
+interface Award {
+    description: string | null;
+    date: string;
+    id: number;
+    category: string;
+    user_id: number;
+    team_id: null;
+    name: string;
+    user: number;
+    team: null;
+    value: number;
+    icon: string;
+}
+
+interface UserSolve {
+    user: {
+        name: string;
+        id: number;
+    };
+    date: string;
+    challenge_id: number;
+    challenge: {
+        value: number;
+        name: string;
+        id: number;
+        category: string;
+    };
+    team: number | null;
+    id: number;
+    type: "correct";
+}
+declare function createUsersMethods(client: CTFdClient): {
+    me: {
+        /**
+         * Retrieves the currently logged-in user.
+         * Ref: {@Link https://docs.ctfd.io/docs/api/redoc#tag/users/operation/get_user_private}
+         * @returns The logged-in user.
+         */
+        get(): Promise<User>;
+        /**
+         * Retrieves solves for the currently logged-in user.
+         * Ref: {@Link https://docs.ctfd.io/docs/api/redoc#tag/users/operation/get_user_private_solves}
+         *
+         * @returns The logged-in user's solves.
+         */
+        getSolves(): Promise<UserSolve[]>;
+        /**
+         * Retrieves awards for the currently logged-in user.
+         * Ref: {@Link https://docs.ctfd.io/docs/api/redoc#tag/users/operation/get_user_private_awards}
+         *
+         * @returns The logged-in user's awards.
+         */
+        getAwards(): Promise<Award[]>;
+    };
+    /**
+     * Retrieves a user by ID.
+     * Ref: {@Link https://docs.ctfd.io/docs/api/redoc#tag/users/operation/get_user_public}
+     *
+     * @param id The ID of the user to retrieve.
+     * @returns The fetched user.
+     */
+    getById(id: number): Promise<User>;
+    /**
+     * Retrieves solves for a given user.
+     * Ref: {@Link https://docs.ctfd.io/docs/api/redoc#tag/users/operation/get_user_public_solves}
+     *
+     * @param id The ID of the user to retrieve solves for.
+     * @returns The user's solves.
+     */
+    getSolves(id: number): Promise<UserSolve[]>;
+    /**
+     * Retrieves awards for a given user.
+     * Ref: {@Link https://docs.ctfd.io/docs/api/redoc#tag/users/operation/get_user_public_awards}
+     *
+     * @param id The ID of the user to retrieve awards for.
+     * @returns The user's awards.
+     */
+    getAwards(id: number): Promise<Award[]>;
+};
+
+type ClientOptions = {
+    url: string;
+    username: string;
+    password: string;
+};
+declare class CTFdClient {
+    readonly url: string;
+    private readonly username;
+    private readonly password;
+    private cachedSession;
+    private cachedNonce;
+    private sessionExpiry;
+    readonly challenges: ReturnType<typeof createChallengesMethods>;
+    readonly scoreboard: ReturnType<typeof createScoreboardMethods>;
+    readonly users: ReturnType<typeof createUsersMethods>;
+    constructor(options: ClientOptions);
+    callApi<T>(endpoint: string): Promise<T>;
+    getAuthedSessionNonce(): Promise<{
+        session: string;
+        nonce: string;
+    }>;
+}
+
+export { type Award, CTFdClient, type Challenge, type ChallengeDetails, type ChallengeType, type ScoreboardEntry, type Solve, type User };
